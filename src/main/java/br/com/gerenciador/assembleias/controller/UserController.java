cd443a13508b28daa4dbb36acbfd69efa0f0aa76
Thiago <Thiago@DESKTOP-7S7EDN4>
2021-06-12 21:58:49 -0300

    criando serviço para validar cpf para depois separar em outro projeto

---
+++ src/main/java/br/com/gerenciador/assembleias/controller/UserController.java
@@ -0,0 +1,36 @@
+package br.com.gerenciador.assembleias.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.caelum.stella.ValidationMessage;
+import br.com.caelum.stella.validation.CPFValidator;
+import br.com.gerenciador.assembleias.model.ResultadoValidaUsuarioEnum;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@Transactional
+	@GetMapping(value = "/{cpf}")
+	public ResponseEntity<?> validaCPF(@PathVariable @Valid String cpf) {
+
+		CPFValidator cpfValidator = new CPFValidator();
+
+		List<ValidationMessage> msgsErro = cpfValidator.invalidMessagesFor(cpf);
+		if (msgsErro.size() == 0) {
+			return ResponseEntity.ok(ResultadoValidaUsuarioEnum.ABLE_TO_VOTE);
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new String("CPF '" + cpf + "' inválido."));
+		}
+	}
+}
---
+++ src/main/java/br/com/gerenciador/assembleias/model/ResultadoValidaUsuarioEnum.java
@@ -0,0 +1,7 @@
+package br.com.gerenciador.assembleias.model;
+
+public enum ResultadoValidaUsuarioEnum {
+	ABLE_TO_VOTE,
+	UNABLE_TO_VOTE;
+
+}
