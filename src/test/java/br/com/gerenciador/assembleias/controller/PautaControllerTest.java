21485e72703124216772324c30f8c52d61fc049f
Thiago <Thiago@DESKTOP-7S7EDN4>
2021-06-13 01:31:03 -0300

    rascunho das classes de teste

---
+++ src/test/java/br/com/gerenciador/assembleias/controller/PautaControllerTest.java
@@ -0,0 +1,29 @@
+package br.com.gerenciador.assembleias.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PautaControllerTest {
+
+	@Test
+	public void testListar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCadastrar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAbrirSessao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDetalhes() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/br/com/gerenciador/assembleias/repository/PautaRepositoryTest.java
@@ -0,0 +1,14 @@
+package br.com.gerenciador.assembleias.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PautaRepositoryTest {
+
+	@Test
+	public void testGetOne() {
+		fail("Not yet implemented");
+	}
+
+}
