50b5b3911b313c7e3dbfa94505509fbe9722d852
Thiago <Thiago@10.124.243.233>
2021-06-14 18:50:58 -0300

    Testes de integração na UserController com 100% de cobertura(coverage)

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ Foi desenvolvido um serviço rest que valida se um CPF é válido e retorna se o
 
 Esse aplicativo está disponível na URL abaixo com documentação Swagger
 * https://thiagobdp-usuarios-cpf.herokuapp.com/swagger-ui.html
+
+## 🔬 Testes automatizados de integração
+
+Foi criado teste automatizado para a classe UserController com 100% de cobertura
+
+Resultando no total de cobertura de teste do sistema em 75,0%.
+
+Utilize Manven Test para executar todos teste juntos. No Eclipse IDE, clique com o botão direito do mouse "pom.xml" -> "Run as" -> "Maven test".
+
+Os testes utilizam o profile "test" e as operações são executadas no banco em memória H2.
--- src/test/java/br/com/gerenciador/usuario/controller/UserControllerTest.java
+++ src/test/java/br/com/gerenciador/usuario/controller/UserControllerTest.java
@@ -1,29 +1,48 @@
 package br.com.gerenciador.usuario.controller;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class UserControllerTest {
 
+	@Autowired
+	private MockMvc mockMvc;
+	
 	@Test
-	public void testListar() {
-		fail("Not yet implemented");
+	public void testValidaCPFValido() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders//
+				.get("/users/{cpf}", "41121167721")//
+				.contentType(MediaType.APPLICATION_JSON))//
+				.andExpect(MockMvcResultMatchers//
+						.status()//
+						.isOk())
+				.andReturn();
+		
 	}
-
+	
 	@Test
-	public void testCadastrar() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAbrirSessao() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testDetalhes() {
-		fail("Not yet implemented");
+	public void testValidaCPFInvalido() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders//
+				.get("/users/{cpf}", "141121167721")//
+				.contentType(MediaType.APPLICATION_JSON))//
+				.andExpect(MockMvcResultMatchers//
+						.status()//
+						.isNotFound())
+				.andReturn();
+		
 	}
 
 }
